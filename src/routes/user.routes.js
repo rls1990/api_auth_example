240f41948c129ae1acc3fd09799f25b11c54275b
rls1990 <roilanlauzardosotolongo@gmail.com>
2023-12-13 17:20:31 -0500

    modified:   src/app.js 	modified:   src/controller/user.controller.js 	modified:   src/libs/jwt.js 	modified:   src/routes/user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { authenticateToken, refreshAccessToken } from "../libs/jwt.js";
-import { login, getUsers } from "../controller/user.controller.js";
+import { login, getUsers, verify } from "../controller/user.controller.js";
 
 const router = Router();
 
 router.post("/login", login);
 
 router.get("/users", authenticateToken, getUsers); // Ruta protegida
+
+router.get("/verify", authenticateToken, verify); // Ruta protegida
 
 router.get("/refresh", refreshAccessToken);
 
