764e5d19f3242f2845cb1bdde333ef8d114c5930
rls1990 <roilanlauzardosotolongo@gmail.com>
2023-12-29 12:09:57 -0500

    renamed:    src/config.js -> src/config/config.js 	modified:   src/controller/user.controller.js 	modified:   src/index.js 	renamed:    src/libs/jwt.js -> src/middleware/auth.middleware.js 	modified:   src/routes/user.routes.js 	new file:   src/utils/bcrypt.js 	new file:   src/utils/jwt.js

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,43 +1,32 @@
 import jwt from "jsonwebtoken";
-import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../config.js";
+import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from "../config/config.js";
 
-export const generateAccessToken = (user) => {
-  const accessToken = jwt.sign(user, ACCESS_TOKEN_KEY, {
-    expiresIn: "5m", // Generar un nuevo token de acceso válido por 5 minutos
-  });
-  return accessToken;
-};
-
-export const generateRefreshToken = (user) => {
-  const refreshToken = jwt.sign(user, REFRESH_TOKEN_KEY, {
-    expiresIn: "1d", // Generar un nuevo token de acceso válido por 1 día
-  });
-  return refreshToken;
-};
-
-export const authenticateToken = (req, res, next) => {
+export const authRequired = (req, res, next) => {
   const authorization = req.headers["authorization"].split(" ");
-  const token_type = authorization[0]; //Bearer: para indicar que es un token de acceso
+  //token_type = Bearer: para indicar que es un token de acceso
+  const token_type = authorization[0];
   const accessToken = authorization[1];
 
   if (!accessToken || token_type !== "Bearer")
     return res
       .status(401)
-      .json({ message: "Sin token de acceso, no autorizado" }); // Sin token de acceso, no autorizado
-
+      .json({ message: "Sin token de acceso, no autorizado" });
+  //verificar el token de acceso
   jwt.verify(accessToken, ACCESS_TOKEN_KEY, (err, user) => {
     if (err)
       return res
         .status(403)
-        .json({ message: "Token de acceso inválido o vencido" }); // Token de acceso inválido o vencido
+        .json({ message: "Token de acceso inválido o vencido" });
+
     req.user = user;
     next(); // Pasar al siguiente middleware
   });
 };
 
-export const refreshAccessToken = (req, res) => {
+export const refreshAccessToken = (req, res, next) => {
   const authorization = req.headers["authorization"].split(" ");
-  const token_type = authorization[0]; //Token: para indicar que es un token de actualización
+  //token_type: Token para indicar que es un token de actualización
+  const token_type = authorization[0];
   const refreshToken = authorization[1];
 
   if (!refreshToken || token_type !== "Token")
@@ -53,6 +42,8 @@ export const refreshAccessToken = (req, res) => {
     const accessToken = jwt.sign({ id: user.id }, ACCESS_TOKEN_KEY, {
       expiresIn: "5m", // Generar un nuevo token de acceso válido por 5 minutos
     });
-    res.json({ accessToken, user });
+    req.accessToken = accessToken;
+    req.user = user;
+    next();
   });
 };
